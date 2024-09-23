b89595c6bdbf1ac6374b9841a8b456326a470cd8
keerthana <keerthana@keerthana>
2024-09-23 11:21:56 +0530

    this my  project

---
+++ com.kodbook/src/main/java/com/kodbook/services/PostService.java
@@ -0,0 +1,27 @@
+package com.kodbook.services;
+
+import java.util.List;
+
+
+
+import com.kodbook.entities.Post;
+
+public interface PostService {
+
+	void createPost(Post post);
+	
+
+	List<Post> getAllPosts();
+	Post getPost(Long id);
+	void updatePost(Post post);
+
+
+	List<Post> fetchAllPosts() ;
+		
+	
+	
+		
+		
+	
+
+}
